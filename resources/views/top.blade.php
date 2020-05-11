489dd245a8308fdbfef6b9275598577f1a2ef14a
”伊東淳” <j.style.0519.i@gmail.com>
2020-05-11 22:10:15 +0900

    トップページの作成

---
+++ resources/views/top.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="loginPage">
+  <div class="container">
+    <div class="loginPage_contents">
+    <h1 class="h3 loginPage_contents_title">素敵な家族を見つけませんか？</h1>
+    <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
+    </div>
+  </div>
+</div>
+@endsection
