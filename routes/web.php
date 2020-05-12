78ad6f365bb5437312370aba2e93aff98b1863d0
”伊東淳” <j.style.0519.i@gmail.com>
2020-05-12 20:00:49 +0900

    プロフィール編集機能

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 Route::group(['prefix' => 'users', 'middleware' => 'auth'], function() {
     Route::get('show/{id}', 'UserController@show')->name('users.show');
+    Route::get('edit/{id}', 'UserController@edit')->name('users.edit');
+    Route::post('update/{id}', 'UserController@update')->name('users.update');
 });
 
 Auth::routes();
