c855b6ba99e85da5da59e6faaf852c57bd15db8c
Jun Ito <j.style.0519.i@gmail.com>
2020-05-28 01:04:38 +0900

    タイトル編集

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,7 +6,9 @@
     <div class="loginPage_contents">
     <h1 class="h3 loginPage_contents_title">素敵な家族を見つけませんか？</h1>
 
-    {{-- <img src="url(/public/storage/images/top.png)"> --}}
+    
+    {{-- <img src="/storage/images/top.jpg"> --}}
+    
     <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
     </div>
   </div>
