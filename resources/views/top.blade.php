c4d7e4a645b4494dda88de0cfa5598a33a4e9445
Jun Ito <j.style.0519.i@gmail.com>
2020-06-07 23:23:31 +0900

    top.blade.php編集

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,3 +1,4 @@
+{{-- トップ画面 --}}
 @extends('layouts.layout')
 
 @section('content')
@@ -5,9 +6,6 @@
   <div class="container">
     <div class="loginPage_contents">
     <h1 class="h3 loginPage_contents_title">素敵な家族を見つけませんか？</h1>
-
-    
-    {{-- <img src="/storage/images/top.jpg"> --}}
     
     <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
     </div>
